4dd86f3531433892703938fd991b82fc7de0a6a2
Mahwish Chohdry <mahwish@xavor.com>
2021-08-12 16:36:56 +0500

    commit

---
+++ SuperAdmin.WebUI/ClientApp/build/precache-manifest.8b55a96146428675ca4a91adfd115875.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "506b5e72b7701212432e71a4a0f2af45",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cdc38ce59018a14d83fa",
+    "url": "/static/css/2.d2e20264.chunk.css"
+  },
+  {
+    "revision": "6676c0bff15fd55843c2",
+    "url": "/static/css/main.7c283dde.chunk.css"
+  },
+  {
+    "revision": "cdc38ce59018a14d83fa",
+    "url": "/static/js/2.4038804b.chunk.js"
+  },
+  {
+    "revision": "6676c0bff15fd55843c2",
+    "url": "/static/js/main.986f5ebd.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "01798bc13e33afc36a52f2826638d386",
+    "url": "/static/media/Pe-icon-7-stroke.01798bc1.ttf"
+  },
+  {
+    "revision": "71394c0c7ad6c1e7d5c77e8ac292fba5",
+    "url": "/static/media/Pe-icon-7-stroke.71394c0c.eot"
+  },
+  {
+    "revision": "b38ef310874bdd008ac14ef3db939032",
+    "url": "/static/media/Pe-icon-7-stroke.b38ef310.woff"
+  },
+  {
+    "revision": "c45f7de008ab976a8e817e3c0e5095ca",
+    "url": "/static/media/Pe-icon-7-stroke.c45f7de0.svg"
+  },
+  {
+    "revision": "e23c63f96c422409a65f7654becf5aef",
+    "url": "/static/media/fan-portal-icon03.e23c63f9.png"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "ac5d549ea3abce0379fa6d3d11ba8463",
+    "url": "/static/media/grey-circle.ac5d549e.png"
+  },
+  {
+    "revision": "5929f99600f64092ee2bad773f2e5abd",
+    "url": "/static/media/login-main.5929f996.jpg"
+  },
+  {
+    "revision": "34535b958ad0f0183770a8731b31b4ca",
+    "url": "/static/media/sidebar-3.34535b95.jpg"
+  }
+]);
